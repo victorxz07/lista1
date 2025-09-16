a851f9f69ec51476e6ad83af1503d696ec6d8e7b
victorxz07 <victorspindola07@gmail.com>
2025-09-16 08:57:03 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,30 @@
+print("Usando for:")
+for i in range(1, 11): 
+    print(i)
+print("\nUsando while:")
+contador = 1
+while contador <= 10:
+    print(contador)
+    contador += 1
+
+
+
+print("Usando o laço for:")
+for i in range(5): 
+    print(f"Repetição {i + 1}")
+print("\nUsando o laço while:")
+contador = 1
+while contador <= 5:
+    print(f"Repetição {contador}")
+    contador += 1
+
+
+
+print("Usando o laço for:")
+for i in range(2, 11, 2): 
+    print(i)
+print("\nUsando o laço while:")
+numero = 2
+while numero <= 10:
+    print(numero)
+    numero += 2 
